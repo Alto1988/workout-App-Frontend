4b72124a093edf1a1347b9f829fb068bd93eb027
MrJake2415 <paganjacob66@yahoo.com>
2021-02-21 16:40:46 -0600

    Added stuff that talks to the backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
+import ApolloClient from 'apollo-boost';
 import React from 'react';
-import logo from './logo.svg';
+import { ApolloProvider } from 'react-apollo';
 import './App.css';
+import { UsersInfo } from './Components/UsersInfo';
+
+
+const client = new ApolloClient({
+  uri: 'http://localhost:8000/graphql/',
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div style={{
+      backgroundColor: '#00000008',
+      display: 'flex',
+      justifyContent:'center',
+      alignItems:'center',
+      height: '100vh',
+      flexDirection: 'column',
+    }}>
+        <h2>First Apollo app
+         <span role="img" aria-label="rocket">🚀</span>
+        </h2>
+        <UsersInfo/>
+      </div>
+    </ApolloProvider>
   );
 }
 
---
+++ src/Components/UsersInfo.tsx
@@ -0,0 +1,34 @@
+import { gql } from 'apollo-boost';
+import React from 'react';
+import { useQuery } from 'react-apollo';
+
+
+
+const QUERY_USERS = gql`
+query{
+  users{
+    id
+    userName
+    workoutType
+  }
+}
+`;
+
+
+export const UsersInfo = () => {
+
+  const { data, loading } = useQuery(
+    QUERY_USERS, {
+    pollInterval: 5000
+  }
+  );
+  if(loading) return <p>Loading.......</p>
+  return data?.users.map(({ id, userName, workoutType }) => (
+    <div key={id}>
+      <p>
+        User -       {id}: {userName} <br />
+        Workout Type-{workoutType}
+      </p>
+    </div>
+  ))
+}
